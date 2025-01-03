18377d84473d78fc43d9c1034c432d653f3b1fe2
Marvin709 <marvinhacheme8@gmail.com>
2025-01-03 13:10:39 +0100

    Mise a jour du fichier PremierTest et creation du test de deconnexion

--- tests/PremierTest.php
+++ tests/PremierTest.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
 require "./register.php";
+require "./authentification.php";
+require "./logout.php";
 use PHPUnit\Framework\TestCase ;
 
     class PremierTest extends TestCase{
@@ -20,10 +22,59 @@ use PHPUnit\Framework\TestCase ;
                     ->willReturn($statementMock);
                   
             $registration = new Registration($pdomock);
-            $result =$registration->registerUser("Marvin","ishola80") ==="Inscription reussie !";
+            $result =$registration->registerUser("Marvin","ishola80","pierrick@gmail.com") ==="Inscription reussie !";
             $this->assertFalse($result);
         }
+        public function test_de_connexion_standard(): void {
+            $pdomock= $this ->getMockBuilder(\PDO::class)
+                            ->disableOriginalConstructor()
+                            ->getMock();
+            $statementMock= $this->getMockBuilder(\PDOStatement::class)
+                                 ->disableOriginalConstructor()
+                                 ->getMock();
+            $statementMock  ->expects($this->any())
+                            ->method('execute')
+                            ->willReturn(true);
+            $statementMock ->expects($this->any())
+                           ->method('fetch')
+                           ->willReturn(['id'=>1,'nom_d_utilisateur'=>'Poutine','mot_de_passe'=>password_hash('Suspect91',PASSWORD_DEFAULT)]);
+                      $pdomock ->expects($this->any())
+                               ->method('prepare')
+                               ->willReturn($statementMock);
+            $login = new Authentification($pdomock);
+            $result= $login->loginuser("Poutine","Suspect91");
+            $this->assertTrue($result, "L'utilisateur devrait être authentifié.");
+
+        }
+     
+    protected function setUp(): void
+    {
+        
+        session_start();
+        $_SESSION['username'] = 'testuser';
     }
 
+    public function testUserIsLoggedIn()
+    {
+        $this->assertTrue(isset($_SESSION['username']));
+        $this->assertEquals('testuser', $_SESSION['username']);
+    }
 
+    public function testLogout()
+    {
+        
+        $this->assertTrue(isset($_SESSION['username']));
+    }
+
+    protected function tearDown(): void
+    {
+        session_unset();
+        session_destroy();
+    }
+}
 ?>
+
+                
+    
+
+
